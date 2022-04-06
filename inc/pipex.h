c99b6410a869cfe73a05fef2d1ea437f66809070
nprimo <primo.niccolo@gmail.com>
2022-04-06 14:37:36 +0100

    Quick reset

--- inc/pipex.h
+++ inc/pipex.h
@@ -6,7 +6,7 @@
 /*   By: nprimo <nprimo@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 10:01:04 by nprimo            #+#    #+#             */
-/*   Updated: 2022/04/06 10:28:12 by nprimo           ###   ########.fr       */
+/*   Updated: 2022/04/06 14:37:06 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-int	free_and_exit(int fdes[2]);
-int	get_fd(const char *filepath, int flag);
+size_t	ft_strlen(const char *str);
+char	*ft_strjoin(char const *s1, char const *s2);
 
 #endif
