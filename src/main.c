ce3065f6cd65066f8a5f62ffece0240607260123
nprimo <primo.niccolo@gmail.com>
2022-04-06 10:53:13 +0100

    Update main.c
    Fix typo in get_fd inputs

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: nprimo <nprimo@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 09:59:42 by nprimo            #+#    #+#             */
-/*   Updated: 2022/04/06 10:32:23 by nprimo           ###   ########.fr       */
+/*   Updated: 2022/04/06 10:52:42 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	main(int ac, char **av)
 
 	if (ac > 4)
 	{
-		fdes[0] = get_fd(av[0], O_RDONLY);
+		fdes[0] = get_fd(av[1], O_RDONLY);
 		fdes[1] = get_fd(av[ac - 1], O_WRONLY);
 		if (fdes[0] < 0 || fdes[1] < 0)
 			return (free_and_exit(fdes));
