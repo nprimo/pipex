4e12cea36feb14ed4a1bbfc69b57f7509a951b03
nprimo <primo.niccolo@gmail.com>
2022-04-06 10:53:49 +0100

    Simplify free_and_exit.c
    Remove loop to close file descriptors

--- src/free_and_exit.c
+++ src/free_and_exit.c
@@ -6,7 +6,7 @@
 /*   By: nprimo <nprimo@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 10:19:00 by nprimo            #+#    #+#             */
-/*   Updated: 2022/04/06 10:30:41 by nprimo           ###   ########.fr       */
+/*   Updated: 2022/04/06 10:49:56 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,8 @@
 
 int	free_and_exit(int fdes[2])
 {
-	while (*fdes)
-	{
-		if (*fdes > 0 && close(*fdes) == 0)
-			fdes++;
-		else
-			break ;
-	}
+	close(fdes[0]);
+	close(fdes[1]);
 	exit(1);
 	return (1);
 }
