b5dc6cc43125a8af88bca0ded74cacc0dcfa2dbd
nprimo <primo.niccolo@gmail.com>
2022-04-09 11:10:37 +0100

    Add exec_pipe.c
    Implement simple function to simulate `lc | wc`

--- inc/pipex.h
+++ inc/pipex.h
@@ -6,20 +6,23 @@
 /*   By: nprimo <nprimo@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 10:01:04 by nprimo            #+#    #+#             */
-/*   Updated: 2022/04/06 14:37:06 by nprimo           ###   ########.fr       */
+/*   Updated: 2022/04/09 11:09:45 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 
-#include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/wait.h>
-#include <unistd.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/wait.h>
+# include <unistd.h>
 
-size_t	ft_strlen(const char *str);
-char	*ft_strjoin(char const *s1, char const *s2);
+# define PATH_LS "/bin/ls"
+# define PATH_CAT "/bin/cat"
+# define PATH_WC "/usr/bin/wc"
+
+int	exec_pipe(char **av_in, char **av_out);
 
 #endif
