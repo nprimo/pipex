2527349afeed7797cf4977d64acb9944f199cb5d
nprimo <primo.niccolo@gmail.com>
2022-04-14 15:45:18 +0100

    Add free_split.c
    Update pipex.h accordingly

--- src/free_split.c
+++ src/free_split.c
@@ -1,32 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.h                                            :+:      :+:    :+:   */
+/*   free_split.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nprimo <nprimo@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/06 10:01:04 by nprimo            #+#    #+#             */
-/*   Updated: 2022/04/11 17:36:27 by nprimo           ###   ########.fr       */
+/*   Created: 2022/04/14 15:40:59 by nprimo            #+#    #+#             */
+/*   Updated: 2022/04/14 15:40:59 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PIPEX_H
-# define PIPEX_H
+#include "pipex.h"
 
-# include <fcntl.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <sys/wait.h>
-# include <unistd.h>
-
-# define PATH_LS "/bin/ls"
-# define PATH_CAT "/bin/cat"
-# define PATH_WC "/usr/bin/wc"
-
-int		exec_command(int fd_in, int fd_out, char **av);
-int		exec_simple_pipe(char **av);
-
-char	**ft_split(char const *s, char c);
-void	free_split(char **av);
-
-#endif
+void	free_split(char **av)
+{
+	while (av)
+	{
+		if (*av)
+			free(*av);
+		av++;
+	}
+}
